253c817cdeeed9b6d36299747e4e7379cbc431c5
Shira3yuriharu <76254421+Shira3yuriharu@users.noreply.github.com>
2021-02-21 22:56:50 +0900

    Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, uniqueness: true #←validates :email, presence: trueの記述は内部的にされている為
+  # validates :email, uniqueness: true #←validates :email, presence: trueの記述は内部的にされている為
   validates :lastname, presence: true
   validates :firstname, presence: true
   validates :lastname_kana, presence: true
   validates :firstname_kana, presence: true
   validates :birthday, presence: true
 
-  #アソシエーションは他モデルを作成したら記述する？
+  # VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  # validates :password, format: { with: VALID_PASSWORD_REGEX }
 
+  # アソシエーションは他モデルを作成したら記述する？
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,35 +5,38 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
 
-  describe "ユーザー新規登録" do
-    it "メールアドレスが空だと登録できない" do
+  describe 'ユーザー新規登録' do
+    it 'メールアドレスが空だと登録できない' do
       @user.email = ''
       @user.valid?
-      #binding.pry
+      # binding.pry
       expect(@user.errors.full_messages).to include "Email can't be blank"
     end
-    it "メールアドレスに＠が含まれてないと登録できない" do
-      user = User.new(nickname: 'test', email: 'testcom', password:'UUU888', password_confirmation:'UUU888', lastname:'山田', firstname:'太郎', lastname_kana:'ヤマダ', firstname_kana:'タロウ', birthday:'2018-02-26')
+    it 'メールアドレスに＠が含まれてないと登録できない' do
+      user = User.new(nickname: 'test', email: 'testcom', password: 'UUU888', password_confirmation: 'UUU888', lastname: '山田',
+                      firstname: '太郎', lastname_kana: 'ヤマダ', firstname_kana: 'タロウ', birthday: '2018-02-26')
       user.valid?
-      #binding.pry
-      expect(user.errors.full_messages).to include "Email is invalid"
+      # binding.pry
+      expect(user.errors.full_messages).to include 'Email is invalid'
     end
-    it "パスワードが空だと登録できない" do
+    it 'パスワードが空だと登録できない' do
       @user.password = ''
       @user.valid?
-      #binding.pry
+      # binding.pry
       expect(@user.errors.full_messages).to include "Password can't be blank"
     end
-    it "パスワードが５文字以下だと登録できない" do
-      user = User.new(nickname: 'test', email: 'test@com', password:'UUU88', password_confirmation:'UUU88', lastname:'山田', firstname:'太郎', lastname_kana:'ヤマダ', firstname_kana:'タロウ', birthday:'2018-02-26')
+    it 'パスワードが５文字以下だと登録できない' do
+      user = User.new(nickname: 'test', email: 'test@com', password: 'UUU88', password_confirmation: 'UUU88', lastname: '山田',
+                      firstname: '太郎', lastname_kana: 'ヤマダ', firstname_kana: 'タロウ', birthday: '2018-02-26')
       user.valid?
-      #binding.pry
-      expect(user.errors.full_messages).to include "Password is too short (minimum is 6 characters)"
+      # binding.pry
+      expect(user.errors.full_messages).to include 'Password is too short (minimum is 6 characters)'
     end
-    it "パスワードとパスワード（確認用）の値が一致しないと登録できない" do
-      user = User.new(nickname: 'test', email: 'test@com', password:'UUU888', password_confirmation:'UUU887', lastname:'山田', firstname:'太郎', lastname_kana:'ヤマダ', firstname_kana:'タロウ', birthday:'2018-02-26')
+    it 'パスワードとパスワード（確認用）の値が一致しないと登録できない' do
+      user = User.new(nickname: 'test', email: 'test@com', password: 'UUU888', password_confirmation: 'UUU887', lastname: '山田',
+                      firstname: '太郎', lastname_kana: 'ヤマダ', firstname_kana: 'タロウ', birthday: '2018-02-26')
       user.valid?
-      #binding.pry
+      # binding.pry
       expect(user.errors.full_messages).to include "Password confirmation doesn't match Password"
     end
   end
