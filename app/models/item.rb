14be035012f43d0d290119ff1dbce14bd0e37c65
Shira3yuriharu <76254421+Shira3yuriharu@users.noreply.github.com>
2021-02-25 08:22:04 +0900

    ActiveHash実装（途中）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,13 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
 
 
 with_options presence: true do
   validates :name, {length: {maximum: 40}}
   validates :explain, {length: {maximum: 1000}}
-  validates :category_id
-  validates :status_id
-  validates :method_of_payment_id  
+  validates :category_id, numericality: {other_than: 1}
+  validates :status_id, numericality: {other_than: 1}
+  validates :method_of_payment_id, numericality: {other_than: 1}  
   validates :area_id
   validates :days_required_id 
   validates :price, format: { with: /\A[0-9]+\z/},inclusion: {in: 300..9999999 }
@@ -15,6 +16,9 @@ end
 
   belongs_to :user
   #has_one :purchase_record
-  #has_one_attached :image
-
+  has_one_attached :image
+  belongs_to :category
+  belongs_to :status
+  belongs_to :method_of_payment
+  belongs_to :days_required
 end
