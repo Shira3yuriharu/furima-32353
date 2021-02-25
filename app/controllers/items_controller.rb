17288f810489dcf398ad9bf8d676c5719b5575d0
Shira3yuriharu <76254421+Shira3yuriharu@users.noreply.github.com>
2021-02-26 08:23:48 +0900

    商品出品機能（エラー発生まで）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,13 +3,30 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show, :new]
 
   def index
+    @items = Item.all
   end
 
   def new
     if user_signed_in?
-      @items = Item.new
+      @item = Item.new
     else
       redirect_to user_session_path
+    end
   end
-  
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save!
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :explain, :category_id, :status_id, :method_of_payment_id, :area_id, :days_required_id, :price).merge(user_id: current_user.id)
+    # params.require(:item).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
 end
