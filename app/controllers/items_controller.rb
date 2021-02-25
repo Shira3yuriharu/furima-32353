182aa6a1d4ff3df137307970e856b50f7f96eca8
Shira3yuriharu <76254421+Shira3yuriharu@users.noreply.github.com>
2021-02-25 20:47:42 +0900

    viewファイルのcellection_select実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,15 @@
 class ItemsController < ApplicationController
+
+  before_action :authenticate_user!, except: [:index, :show, :new]
+
   def index
   end
 
   def new
+    if user_signed_in?
+      @items = Item.new
+    else
+      redirect_to user_session_path
   end
+  
 end
