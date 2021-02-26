113876b3ee42b99442261217a9123085378fb762
Shira3yuriharu <76254421+Shira3yuriharu@users.noreply.github.com>
2021-02-27 00:39:28 +0900

    factories(items)設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,9 +10,11 @@ with_options presence: true do
   validates :method_of_payment_id, numericality: {other_than: 1, message: "Select"}
   validates :area_id, numericality: {other_than: 1, message: "Select"}
   validates :day_required_id , numericality: {other_than: 1, message: "Select"}
-  validates :price, format: { with: /\A[0-9]+\z/, message: "Half-width number"}, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
+  validates :price, format: {with: /\A[0-9]+\z/, message: "Half-width number"}
   validates :image
 end
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
+
 
   belongs_to :user
   #has_one :purchase_record
