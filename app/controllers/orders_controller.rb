1dc8614f93e96f3d1904ab268d84cea6c4ddefd4
Shira3yuriharu <76254421+Shira3yuriharu@users.noreply.github.com>
2021-03-12 12:54:03 +0900

    購入画面の商品名称など表示完了

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,24 @@
 class OrdersController < ApplicationController
   def index
-    @order = UserPurchase.new
+    @user_purchase = UserPurchase.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
-    @user_purchase = UserPurchase.new
-    binding.pry
+    @user_purchase = UserPurchase.new(order_params)
+    # binding.pry
+     if @user_purchase.valid?
+      @user_purchase.save
+      redirect_to root_path
+     else
+      render action: :index
+     end
+  end
+
+  private
+  def order_params
+    # binding.pry
+    params.require(:user_purchase).permit(:purchase_record, :post_code, :area_id, :city, :address, :building_name, :phone_number, :item_id).merge( user_id: current_user.id)
   end
 
 end
