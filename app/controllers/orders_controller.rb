910bc3625ae137f11d1f5fc59147b9306bca40a7
Shira3yuriharu <76254421+Shira3yuriharu@users.noreply.github.com>
2021-03-13 00:32:57 +0900

    ordersコントローラー_リファクタリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,13 +9,7 @@ class OrdersController < ApplicationController
     @user_purchase = UserPurchase.new(user_purchase_params)
     @item = Item.find(params[:item_id])
      if @user_purchase.valid?
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: user_purchase_params[:token],
-        currency: 'jpy'
-      )
-      # binding.pry
+      pay_item
       @user_purchase.save
       redirect_to root_path
      else
@@ -23,10 +17,21 @@ class OrdersController < ApplicationController
      end
   end
 
+
   private
+
   def user_purchase_params
     # binding.pry
     params.require(:user_purchase).permit(:post_code, :area_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: user_purchase_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
