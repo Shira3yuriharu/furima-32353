8683fe1072d7922f9844768a21885e5de7e07543
Shira3yuriharu <76254421+Shira3yuriharu@users.noreply.github.com>
2021-03-13 00:25:19 +0900

    token_validation設定

--- app/models/user_purchase.rb
+++ app/models/user_purchase.rb
@@ -4,12 +4,13 @@ class UserPurchase
   attr_accessor :purchase_record, :post_code, :area_id, :city, :address, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :user_id
-    validates :item_id
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :city, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters." }
     validates :address
     validates :phone_number, format: { with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid." }
+    validates :user_id
+    validates :item_id
+    validates :token
   end
     validates :area_id, numericality: { other_than: 0, message: "can't be blank" }
     
