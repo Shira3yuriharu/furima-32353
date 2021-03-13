2938e5bd9bcf4ae48e551d619994191826080c3c
Shira3yuriharu <76254421+Shira3yuriharu@users.noreply.github.com>
2021-03-13 14:03:47 +0900

    テストコード実装完了

--- app/models/user_purchase.rb
+++ app/models/user_purchase.rb
@@ -4,15 +4,16 @@ class UserPurchase
   attr_accessor :purchase_record, :post_code, :area_id, :city, :address, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
-    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters." }
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "input correctly. And also need hyphen(-)." }
+    validates :area_id, numericality: { other_than: 1, message: 'Select' }
+    validates :city
+    # validates :city, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "can't be blank." }
     validates :address
-    validates :phone_number, format: { with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid." }
+    validates :phone_number, format: { with: /\A\d{10}\z|\A\d{11}\z/ , message: "input only number.  And also within 11 digits." }
     validates :user_id
     validates :item_id
     validates :token
   end
-    validates :area_id, numericality: { other_than: 0, message: "can't be blank" }
     
   def save
     purchaserecord = PurchaseRecord.create(user_id: user_id, item_id: item_id)
