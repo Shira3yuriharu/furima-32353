745a231ef3e418273f661aa70795c095bcca970b
Shira3yuriharu <76254421+Shira3yuriharu@users.noreply.github.com>
2021-03-13 09:20:36 +0900

    factorybot作成

--- spec/factories/user_purchases.rb
+++ spec/factories/user_purchases.rb
@@ -1,12 +1,13 @@
 FactoryBot.define do
   factory :user_purchase do
-    post_code
+    post_code        { Randwordjp.zip(opts = {hyphen: true }) }
     area_id          { Faker::Number.between(from: 2, to: 48) }
-    city
-    address
-    building_name
-    phone_number
-
-
+    city             { Gimei.address.city.kanji }
+    address          { Gimei.town.kanji + '1-1-1'}
+    building_name    { Gimei.town.kanji + 'ビル101号室'}
+    phone_number     { '090' + Faker::Number.(digits: 8) }
+    user_id          {Faker::Number.number(digits: 1) }
+    item_id          {Faker::Number.number(digits: 1) }
+    token            {"tok_abcdefghijk00000000000000000"}
   end
 end
